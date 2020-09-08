f74dd3701cc8a3e80b3dc31ed7afad9cfd051520
oldanderw <adparker@fullsail.edu>
2020-09-08 00:50:51 -0400

    adding flies

---
+++ view.js
@@ -0,0 +1,45 @@
+const $ = require('jquery')
+const fs = require('fs')
+const filename = 'contacts'
+let sno = 0
+
+$('#add-to-list').on('click', () => {
+  let name = $('#Name').val()
+  let email = $('#Email').val()
+
+  fs.appendFile('contacts', name + ',' + email + '\n')
+
+  addEntry(name, email)
+})
+
+function addEntry(name, email) {
+  if (name && email) {
+    sno++
+    let updateString = `<tr><td>${sno}</td><td>${name}</td><td>${email}</td></tr>`
+    $('#contact-table').append(updateString)
+  }
+}
+//$('#loadAndDisplayContacts').on('click', () => {
+//  loadAndDisplayContacts()
+//})
+function loadAndDisplayContacts() {
+
+  //Check if file exists
+  if (fs.existsSync(filename)) {
+    let data = fs.readFileSync(filename, 'utf8').split('\n')
+
+    data.forEach((contact, index) => {
+      let [ name, email ] = contact.split(',')
+      addEntry(name, email)
+    })
+
+  } else {
+    console.log('File dont Exist. Creating new file.')
+    fs.writeFile(filename, '', (err) => {
+      if (err)
+         console.log(err)
+    })
+  }
+}
+
+loadAndDisplayContacts()
